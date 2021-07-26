f54a6c7a7155beb89248ac19b30dbd71cc787d1f
Anatoly Scherbakov <altaisoft@gmail.com>
2021-07-26 19:24:37 +0700

    Create the named context

--- octadocs_decisions/plugin.py
+++ octadocs_decisions/plugin.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from mkdocs.plugins import BasePlugin
+from octadocs.octiron.context_loaders import context_from_yaml
 
 
 class DecisionsPlugin(BasePlugin):
@@ -11,6 +12,19 @@ class DecisionsPlugin(BasePlugin):
         """Templates associated with the plugin."""
         return Path(__file__).parent / 'templates'
 
+    def load_context(self):
+        """Load YAML-LD context."""
+        return context_from_yaml(Path(__file__).parent / 'yaml/context.yaml')
+
     def on_config(self, config, **kwargs):
-        """Make the plugin's templates available to MkDocs."""
+        """Adjust configuration."""
+        # Make plugin's templates available to MkDocs
         config['theme'].dirs.append(str(self.templates_path))
+
+        named_context = self.load_context()
+        try:
+            contexts = config['extra']['named_contexts']
+        except KeyError:
+            contexts = config['extra']['named_contexts'] = {}
+
+        contexts['octadocs_decisions'] = named_context
